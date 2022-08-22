b17756ff6880841b7cd7e275cc52a194a2415fa1
Christian <christianvillere@gmail.com>
2022-08-21 17:51:23 -0700

    Initial Commit

---
+++ main.rb
@@ -0,0 +1,12 @@
+#this is the class for making the gameboard, knight, and potential moves
+class GameBoard
+
+  @attr_accessor :board, :knight
+
+  def initialize
+    @board = 2d array
+    @knight = knight
+  end
+  
+
+end
