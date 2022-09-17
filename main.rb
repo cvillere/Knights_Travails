4ed14c2f9ad3787f38f949e7fc6ca98e24f84503
Christian <christianvillere@gmail.com>
2022-09-16 18:08:02 -0700

    Add find_move_position func to Gameboard class Function added to reach each position on the board

--- main.rb
+++ main.rb
@@ -1,12 +1,65 @@
-#this is the class for making the gameboard, knight, and potential moves
+# this is the class for making the gameboard, knight, and potential moves
 class GameBoard
 
-  @attr_accessor :board, :knight
+  attr_accessor :board, :knight
 
   def initialize
-    @board = 2d array
+    @board = Array.new(8) { Array.new(8)}
     @knight = knight
   end
+
+  def find_move_position
+    knight_moves = [[2, -1], [2, 1], [1, -2], [1, 2], [1, -2], [-1, 2], [-2, -1], [-2, 1]]
+    # serve up each position on the board (loop)
+    board.each_with_index do |row, row_index|
+      row.each_with_index do |column, col_index|
+        knight_moves.each do |km|
+          position_on_board([row_index, col_index], km)
+        end
+      end
+    end
+  end
+
+end
+
+
+
+
+=begin
+#Not ready to commit just yet
+
+# Graph node of the graph
+class GraphNode
+
+  attr_accessor :value, :neighbors
+
+  def initialize(value)
+    @value = value
+    @neighbors = []
+  end
+
+  def add_edge(neighbor)
+    @neighbors << neighbor
+  end
+
+end
+
+# class representing graph object
+class Graph
+
+  attr_accessor :nodes
+
+  def initialize
+    @nodes = []
+  end
+
+  def add_node(value)
+    @nodes << GraphNode.new(value)
+  end
+
   
 
 end
+
+=end
+
